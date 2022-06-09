da08edd54e93a354ee328f8ff1f445fa2f8de316
Nhat Hoang <johnhoanghtx@gmail.com>
2022-06-09 16:01:36 -0500

    DevOpLab

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const controllerFile = require("./controller");
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('public'))
 
 var Rollbar = require("rollbar");
 var rollbar = new Rollbar({
@@ -23,7 +24,6 @@ app.delete("/api/albums/:id", controllerFile.deleteAlbum);
 app.post("/api/albums", controllerFile.createAlbum);
 
 
-app.use(express.static('public'))
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
