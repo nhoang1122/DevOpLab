905c6a1e2f3c994e50a9c9ce74e1838cc5bb6a46
Nhat Hoang <johnhoanghtx@gmail.com>
2022-06-09 14:46:27 -0500

    DevOpLab

--- server/controller.js
+++ server/controller.js
@@ -1,4 +1,10 @@
 const albums = require('./db.json');
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: '1bd34d8fc4db4dc7ad17f2d7a8e3d496',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
 
 module.exports = {
     getAlbum: (req,res) => {
